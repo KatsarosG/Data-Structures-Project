d8363cbc266cc6e914d8c345dc8cfd6b044b378c
GiannisKotsalos <kotsalos.gi@gmail.com>
2024-05-15 13:11:33 +0300

    HeapSort and CalcTotalDeaths

--- sortingFunctions.cpp
+++ sortingFunctions.cpp
@@ -71,3 +71,39 @@ void mergeSort(Region arr[], int l, int r) {
 		merge(arr, l, m, r);
 	}
 }
+void heapify(Region regionArray[], int n, int i) {
+    int largest = i; // Initialize largest as root
+    int left = 2 * i + 1; // left = 2*i + 1
+    int right = 2 * i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (left < n && regionArray[left].totalDeaths > regionArray[largest].totalDeaths)
+        largest = left;
+
+    // If right child is larger than largest so far
+    if (right < n && regionArray[right].totalDeaths > regionArray[largest].totalDeaths)
+        largest = right;
+
+    // If largest is not root
+    if (largest != i) {
+        swap(regionArray[i], regionArray[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(regionArray, n, largest);
+    }
+}
+
+void heapSort(Region regionArray[], int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(regionArray, n, i);
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i >= 0; i--) {
+        // Move current root to end
+        swap(regionArray[0], regionArray[i]);
+
+        // call max heapify on the reduced heap
+        heapify(regionArray, i, 0);
+    }
+}
