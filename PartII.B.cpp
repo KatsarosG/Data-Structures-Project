b0ee4740f521ea753e5cf885ba8062647eb16401
KatsarosG <giorgoskgkg@gmail.com>
2024-06-04 14:02:44 +0300

    partII B nearly finished

--- PartII.B.cpp
+++ PartII.B.cpp
@@ -9,6 +9,8 @@ ifstream DataFile("Data(Edited).txt");
 Row dataArray[NUMOFENTRIES];
 Region regionArray[NUMOFREGIONS];
 vector<Node> nodeVector(NUMOFREGIONS);
+void Question1(int);
+void Question2(int);
 
 int main() {
 	clock_t clockStart, clockEnd;
@@ -24,7 +26,11 @@ int main() {
 	nodeVector = makeNodeVector(regionArray);
 	makeTree(nodeVector, "births");
 
+	Question1(0);
+	Question2(0);
+
 	// print tree
+	/*
 	cout << "\n----------\n";	
 	for (int i = 0; i < nodeVector.size(); i++) {
 		cout << i << ": " << nodeVector[i].region.name << ": " << nodeVector[i].region.totalBirths << ": " << endl;
@@ -32,7 +38,24 @@ int main() {
 		cout << "\tLeft: " << nodeVector[i].left << endl;
 		cout << "\tRight: " << nodeVector[i].right << endl;
 	}
+	*/
 	
 	
 	return 0;
+}
+
+void Question1(int i) {
+	if (nodeVector[i].left == -1) {
+		cout << "Region with least total births: " << nodeVector[i].region.name << " with " << nodeVector[i].region.totalBirths << " total births." << endl;
+	} else {
+		Question1(nodeVector[i].left);
+	}
+}
+
+void Question2(int i) {
+	if (nodeVector[i].right == -1) {
+		cout << "Region with most total births: " << nodeVector[i].region.name << " with " << nodeVector[i].region.totalBirths << " total births." << endl;
+	} else {
+		Question2(nodeVector[i].right);
+	}
 }
